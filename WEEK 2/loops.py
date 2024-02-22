de53fb1636cf4f6fa05fdc68610d57bec7c7e6a4
samuelkimaru <samuelkimaru833@gmail.com>
2024-02-22 10:10:35 +0300

    lesson on strings

---
+++ WEEK 2/loops.py
@@ -0,0 +1,15 @@
+# loops and iterations
+# date : 22/2/2024
+# name : muchiri
+ 
+# for loop
+# while loop/do while
+
+for number in range(0,10):
+    print(number)
+    # squares
+    for x in range(0,11):
+     print (str(x**2)  + "\t",end=" ") # squares
+   
+
+
---
+++ WEEK 2/strings.py
@@ -0,0 +1,50 @@
+# strings in python
+# date : 22/2/2024
+# name : muchiri
+
+
+
+city = "nairobi"
+
+# how to convert to uppercase
+print (city.upper())
+print (city)
+print ("\n\n") # prints a new line
+
+
+name = "MUCHIRI"
+print(name)
+print (name.lower()) # this converts string to lower case
+
+town = "   Naivasha  "
+print(town)
+print("\t") # new tab
+print(town.strip())
+# add two strips
+f_name = "samuel"
+s_name = "muchiri"
+full_name = f_name + s_name
+print(full_name)
+
+
+print(city[0])
+print(city[1])
+print(city[2])
+print(city[3])
+print(city[4])
+print(city[5])
+print(city[6])
+
+
+# replacing a character
+fruit = "orange"
+print (fruit.replace("o","y"))
+subject = "physical:science"
+print(subject.split(":"))
+age = 30
+height = 1.2
+print("i am {0} years old and {1} meters tall" .format(age,height))
+
+
+
+
